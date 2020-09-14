b9843cf00b3f47db9528e60c3151f0f562fc328b
David M <sloshedtrain@gmail.com>
2020-09-14 00:39:44 -0600

    Made some progress on the other commands

--- shell/shell.py
+++ shell/shell.py
@@ -3,15 +3,16 @@ import sys
 import re 
 
 def shell_input():
-	u_str = input()
-	args = u_str.split(" ")
+	args = user_input()
 
 	for i in range(len(args)):
 		if(args[i] == '&'):
-			print("background tasks here")
+			background_u(args)
+			return
 
 		elif(args[i] == '|'):
-			print("pipes go here")
+			pipe_u(args)
+			return
 
 		elif(args[i] == '<'):
 			print("redirect go here")
@@ -20,19 +21,81 @@ def shell_input():
 			print("redirect go here too")
 
 		elif(args[i] == 'exit'):
-			sys.exit(0)
+			sys.exit(1)
 
 	exec_u(args)
 
-def exec_u(args):
+def pipe_u(args):
+	pid = os.getpid()               # get and remember pid
+	pr, pw = os.pipe()
+
+	for f in (pr, pw):
+		os.set_inheritable(f, True)
+
+	import fileinput
+
+	rc = os.fork()
+
+	if rc < 0:
+		sys.exit(1)
+
+	elif rc == 0:                   #  child - will write to pipe
+		os.close(1)                 # redirect child's stdout
+		os.dup(pw)
+		for fd in (pr, pw):
+			os.close(fd)
+
+	else:                           # parent (forked ok)
+		os.close(0)
+		os.dup(pr)
+		for fd in (pw, pr):
+			os.close(fd)
+
+		for line in fileinput.input():
+			print("From child: <%s>" % line)
+	
+	shell_input()
+
+def background_u(args):
+	rc = os.fork()
+
 	for dir in re.split(":", os.environ['PATH']):
 		program = "%s/%s" % (dir, args[0])
-		try:
-			os.execve(program, args, os.environ) # try to exec program
-		except FileNotFoundError:             # ...expected
-			continue                              # ...fail quietly
+		if rc == 0:
+			try:
+				os.execve(program, args, os.environ) # try to exec program
+			except FileNotFoundError:             # ...expected
+				continue                              # ...fail quietly
+		else:
+			shell_input()
+			return
 
-	
+def exec_u(args):
+	pid = os.getpid()
+	rc = os.fork()
+
+	for dir in re.split(":", os.environ['PATH']):
+		program = "%s/%s" % (dir, args[0])
+		if rc < 0:
+			print("fork failed. Try again.")
+			sys.exit(1)
+		elif rc == 0:
+			try:
+				os.execve(program, args, os.environ) # try to exec program
+			except FileNotFoundError:             # ...expected
+				continue                              # ...fail quietly
+		elif rc > 0:
+			childPidCode = os.wait()
+			shell_input()
+			return
+
+	print(args[0] + " not found")
+
+def user_input():
+	u_str = input()
+	args = u_str.split(" ")
+	return args
+
 def main():
 	shell_input()
 
